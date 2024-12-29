37381e922bed0341f80eb7335a88f25d0d381aeb
Huy <tranthanhhuylxagg@gmail.com>
2024-12-29 21:45:36 +0700

    update

--- app/Models/SanPham.php
+++ app/Models/SanPham.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SanPham extends Model
 {
-    //
+    protected $table = 'sanpham';
+
+    public function LoaiSanPham(): BelongsTo{
+        return $this->belongsTo(LoaiSanPham::class, 'loaisanpham_id', 'id');
+    }
+   
+    public function ThuongHieu(): BelongsTo{
+        return $this->belongsTo(HangSanXuat::class, 'thuonghieu_id', 'id');
+    }
+   
+    public function DonHang_ChiTiet(): HasMany{
+        return $this->hasMany(DonHang_ChiTiet::class, 'sanpham_id', 'id');
+    }
 }
--- app/Models/ThuongHieu.php
+++ app/Models/ThuongHieu.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ThuongHieu extends Model
 {
-    //
+    protected $table = 'thuonghieu';
+
+    public function SanPham(): HasMany{
+    return $this->hasMany(SanPham::class, 'thuonghieu_id', 'id');
+    }
 }
