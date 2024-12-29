be9c724440cbb0ea87793f3e2c1b515e47a940b6
Huy <tranthanhhuylxagg@gmail.com>
2024-12-29 20:20:09 +0700

    database

--- database/migrations/2024_12_29_130528_create_loai_san_phams_table.php
+++ database/migrations/2024_12_29_130528_create_loai_san_phams_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('loai_san_phams', function (Blueprint $table) {
             $table->id();
+            $table->string('tenloai');
+            $table->string('tenloai_slug');
             $table->timestamps();
         });
     }
@@ -22,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('loai_san_phams');
+        Schema::dropIfExists('loai_san_pham');
     }
 };
